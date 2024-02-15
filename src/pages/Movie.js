fea7d8b392e703d9ad66feaf23468daa0bf92bd7
kent riggs <k2r3@proton.me>
2024-02-15 13:02:50 -0700

    added code to satisfy tests

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,16 +1,39 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Home from "./Home";
 
 function Movie() {
+  const [movie, setMovie] = useState(null);
+  const params = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${params.id}`)
+    .then(r => r.json())
+    .then(data => setMovie(data))
+    .catch((error) => console.error(error));
+  }, [params.id]);
+
+  if (!movie) {
+    return <div>Loading...</div>
+  }
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <Home />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>Time: {movie.time}</p>
+        <div>
+        Genres:{""}
+          {movie.genres.map((genre, index) => (
+            <span key={index}>{genre}</span>
+          ))}
+        </div>
       </main>
     </>
   );
-};
+}
 
-export default Movie;
+export default Movie
